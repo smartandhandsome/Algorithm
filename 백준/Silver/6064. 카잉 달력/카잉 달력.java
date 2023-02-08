d6d3dbcb618ad233757cb7c635b69d79e7b6923b
Sangmin <70051888+smart-sangmin@users.noreply.github.com>
2023-02-09 01:41:10 +0900

    [Silver I] Title: 카잉 달력, Time: 644 ms, Memory: 41344 KB -BaekjoonHub

---
+++ 백준/Silver/6064. 카잉 달력/카잉 달력.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        for (int i = 0; i < T; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int M = Integer.parseInt(st.nextToken());
+            int N = Integer.parseInt(st.nextToken());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+
+            int GCD = gcd(M, N);
+            int LCM = (M * N) / GCD;
+            int answer = -1;
+
+            while (LCM >= x) {
+                if ((x - y) % N == 0) {
+                    answer = x;
+                    break;
+                }
+                x += M;
+            }
+            System.out.println(answer);
+        }
+    }
+
+    public static int gcd(int a, int b) {
+        if (b == 0) {
+            return a;
+        }
+        return gcd(b, a % b);
+    }
+}
