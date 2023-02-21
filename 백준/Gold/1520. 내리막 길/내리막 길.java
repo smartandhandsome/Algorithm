3d1329e9cbdc06e1820b33ae19baaa3ef156a04a
Sangmin Park <70051888+smart-sangmin@users.noreply.github.com>
2023-02-21 11:56:36 +0900

    Update 내리막 길.java

--- 백준/Gold/1520. 내리막 길/내리막 길.java
+++ 백준/Gold/1520. 내리막 길/내리막 길.java
@@ -22,28 +22,55 @@ public class Main {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < M; j++) {
                 map[i][j] = Integer.parseInt(st.nextToken());
-                dp[i][j] = -1;
+//                dp[i][j] = -1;
             }
         }
-        System.out.print(dfs(0, 0));
+//        System.out.print(dfs(0, 0));
+        System.out.print(bfs());
     }
 
-    public static int dfs(int row, int col) {
-        if (row == N - 1 && col == M - 1) {
-            return 1;
-        }
-        if (dp[row][col] != -1) {
-            return dp[row][col];
-        }
-        dp[row][col] = 0;
-        for (int i = 0; i < 4; i++) {
-            int movedRow = row + moveRow[i];
-            int movedCol = col + moveCol[i];
-            if (0 <= movedRow && movedRow < N && 0 <= movedCol && movedCol < M &&
-                    map[movedRow][movedCol] < map[row][col]) {
-                dp[row][col] += dfs(movedRow, movedCol);
+//    public static int dfs(int row, int col) {
+//        if (row == N - 1 && col == M - 1) {
+//            return 1;
+//        }
+//        if (dp[row][col] != -1) {
+//            return dp[row][col];
+//        }
+//        dp[row][col] = 0;
+//        for (int i = 0; i < 4; i++) {
+//            int movedRow = row + moveRow[i];
+//            int movedCol = col + moveCol[i];
+//            if (0 <= movedRow && movedRow < N && 0 <= movedCol && movedCol < M &&
+//                    map[movedRow][movedCol] < map[row][col]) {
+//                dp[row][col] += dfs(movedRow, movedCol);
+//            }
+//        }
+//        return dp[row][col];
+//    }
+
+    public static int bfs() {
+        PriorityQueue<int[]> q = new PriorityQueue<>(new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                return map[o2[0]][o2[1]] - map[o1[0]][o1[1]];
+            }
+        });
+        q.add(new int[]{0, 0});
+        dp[0][0] = 1;
+        while (!q.isEmpty()) {
+            int[] temp = q.poll();
+            for (int i = 0; i < 4; i++) {
+                int movedRow = temp[0] + moveRow[i];
+                int movedCol = temp[1] + moveCol[i];
+                if (0 <= movedRow && movedRow < N && 0 <= movedCol && movedCol < M &&
+                        map[movedRow][movedCol] < map[temp[0]][temp[1]]) {
+                    if (dp[movedRow][movedCol] == 0) {
+                        q.add(new int[]{movedRow, movedCol});
+                    }
+                    dp[movedRow][movedCol] += dp[temp[0]][temp[1]];
+                }
             }
         }
-        return dp[row][col];
+        return dp[N - 1][M - 1];
     }
 }
