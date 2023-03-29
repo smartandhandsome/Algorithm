4be58628ef2261c94775fd21a4274f31ebf2d8f7
Sangmin Park <70051888+smart-sangmin@users.noreply.github.com>
2023-03-29 14:24:05 +0900

    [Silver I] Title: 경로 찾기, Time: 396 ms, Memory: 18676 KB -BaekjoonHub

---
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main{
+    static int N;
+    static int[][] maps;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        maps = new int[N][N];
+        StringTokenizer st;
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
+                maps[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        int[][] answer = new int[N][N];
+        for (int k = 0; k < N; k++) {
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < N; j++) {
+                    if (maps[i][j] == 1 || (answer[i][k] == 1 && answer[k][j] == 1)) {
+                        answer[i][j] = 1;
+                    }
+                }
+            }
+        }
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                System.out.print(answer[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
