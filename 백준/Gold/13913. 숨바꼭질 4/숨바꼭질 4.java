281f5bca09dddaa95942149bae36080bd0707a53
Sangmin Park <70051888+smart-sangmin@users.noreply.github.com>
2023-04-13 08:58:46 +0900

    [Gold IV] Title: 숨바꼭질 4, Time: 328 ms, Memory: 32424 KB -BaekjoonHub

--- 백준/Gold/13913. 숨바꼭질 4/숨바꼭질 4.java
+++ 백준/Gold/13913. 숨바꼭질 4/숨바꼭질 4.java
@@ -1,48 +1,90 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+import java.util.StringTokenizer;
 
+// 숨바꼭질 4 문제 - 13913번
 public class Main {
+
+    static int N, K;
+    static int time;
+    static boolean[] visited = new boolean[100001];
+    static int[] parent = new int[100001];
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        String N = st.nextToken();
-        int K = Integer.parseInt(st.nextToken());
-        if (Integer.parseInt(N) > K) {
-            int temp = Integer.parseInt(N);
-            System.out.println(temp - K);
-            while (temp-- >= K) {
-                System.out.print((temp+1) + " ");
+        StringBuilder sb = new StringBuilder();
+
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+
+        if (N == K) {
+            sb.append(0).append("\n");
+            sb.append(N).append("\n");
+        } else {
+            bfs(N);
+            sb.append(time).append("\n");
+
+            Stack<Integer> stack = new Stack<>();
+            stack.add(K);
+            int index = K;
+            while (index != N) {
+                stack.push(parent[index]);
+                index = parent[index];
             }
-            System.exit(0);
+
+            while (!stack.isEmpty()) {
+                sb.append(stack.pop()).append(" ");
+            }
         }
-        Queue<String[]> q = new LinkedList<>();
-        boolean[] visited = new boolean[100001]; // bfs 메모리 초과 시 visited 함수 쓰기
-        visited[Integer.parseInt(N)] = true;
-        q.add(new String[]{N, N});
+        System.out.println(sb);
+    }
+
+    static void bfs(int start) {
+        time = 0;
+        Queue<Integer> q = new LinkedList<>();
+        q.offer(start);
+        visited[start] = true;
+
         while (!q.isEmpty()) {
-            String[] ss = q.poll();
-            int pos = Integer.parseInt(ss[0]);
-            String path = ss[1];
-            if (pos == K) {
-                st = new StringTokenizer(path);
-                System.out.println(st.countTokens() - 1);
-                while (st.hasMoreTokens()) {
-                    System.out.print(st.nextToken() + " ");
+            // 만난 경우
+            if (visited[K]) {
+                return;
+            }
+
+            // 현재 q의 사이즈 만큼만 돌리기 - 시간 계산을 위해서
+            for (int j = 0, size = q.size(); j < size; j++) {
+                int now = q.poll();
+                int next;
+
+                // 다음에서 영역을 방문 처리
+                // 다음의 부모를 현재로 설정
+                next = now - 1;
+                if (next >= 0 && !visited[next]) {
+                    visited[next] = true;
+                    parent[next] = now;
+                    q.offer(next);
                 }
-                break;
+
+                next = now + 1;
+                if (next < 100001 && !visited[next]) {
+                    visited[next] = true;
+                    parent[next] = now;
+                    q.offer(next);
+                }
+
+                next = now * 2;
+                if (next < 100001 && !visited[next]) {
+                    visited[next] = true;
+                    parent[next] = now;
+                    q.offer(next);
+                }
             }
-            if (pos * 2 <= 100000 && pos * 2 >= 0 && !visited[pos * 2]) {
-                q.add(new String[]{String.valueOf(pos * 2), path + " " + (pos * 2)});
-                visited[pos * 2] = true;
-            }
-            if (pos - 1 <= 100000 && pos - 1 >= 0 && !visited[pos - 1]) {
-                q.add(new String[]{String.valueOf(pos - 1), path + " " + (pos - 1)});
-                visited[pos - 1] = true;
-            }
-            if (pos + 1 <= 100000 && pos + 1 >= 0 && !visited[pos + 1]) {
-                q.add(new String[]{String.valueOf(pos + 1), path + " " + (pos + 1)});
-                visited[pos + 1] = true;
-            }
+            time++;
         }
     }
 }
