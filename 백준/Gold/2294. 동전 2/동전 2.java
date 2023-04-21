fae1cdabdd7a24c77cb006c115c5deb03f4df1e7
Sangmin Park <70051888+smart-sangmin@users.noreply.github.com>
2023-04-21 12:43:24 +0900

    [Gold V] Title: 동전 2, Time: 136 ms, Memory: 14248 KB -BaekjoonHub

---
+++ 백준/Gold/2294. 동전 2/동전 2.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        
+        int[] coin = new int[N];
+        for (int i = 0; i < N; i++) {
+            coin[i] = Integer.parseInt(br.readLine());
+        }
+        
+        int[] dp = new int[K+1];
+        Arrays.fill(dp, Integer.MAX_VALUE-1);
+        dp[0] = 0;
+        for (int i = 0; i < N; i++) {
+            for (int j = coin[i]; j <= K; j++){
+                dp[j] = Math.min(dp[j], dp[j - coin[i]]+1);
+            }
+        }
+        System.out.println(dp[K] == Integer.MAX_VALUE-1 ? -1 : dp[K]);
+    }
+}
