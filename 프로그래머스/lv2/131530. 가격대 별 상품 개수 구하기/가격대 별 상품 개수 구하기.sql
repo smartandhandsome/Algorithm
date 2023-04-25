b62307fd6baff8aa25b41fa895db13d8c7b3cf57
Sangmin Park <70051888+smart-sangmin@users.noreply.github.com>
2023-04-25 16:31:49 +0900

    [level 2] Title: 가격대 별 상품 개수 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv2/131530. 가격대 별 상품 개수 구하기/가격대 별 상품 개수 구하기.sql
+++ 프로그래머스/lv2/131530. 가격대 별 상품 개수 구하기/가격대 별 상품 개수 구하기.sql
@@ -1,4 +1,4 @@
-SELECT TRUNCATE(PRICE, -4), COUNT(*)
-FROM PRODUCT
-GROUP BY 1
-ORDER BY 1;
+select truncate(price / 10000, 0) * 10000 PRICE_GROUP, count(*) PRODUCTS
+from PRODUCT
+group by (truncate(price / 10000, 0) * 10000)
+order by 1
