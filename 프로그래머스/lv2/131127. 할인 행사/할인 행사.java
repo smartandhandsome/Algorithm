a882e6f9742342fda28963d10be579dbbafc198e
Sangmin Park <70051888+smart-sangmin@users.noreply.github.com>
2023-04-28 11:47:05 +0900

    [level 2] Title: 할인 행사, Time: 0.03 ms, Memory: 69.7 MB -BaekjoonHub

--- 프로그래머스/lv2/131127. 할인 행사/할인 행사.java
+++ 프로그래머스/lv2/131127. 할인 행사/할인 행사.java
@@ -1,28 +1,6 @@
-import java.util.*;
-
 class Solution {
     public int solution(String[] want, int[] number, String[] discount) {
         int answer = 0;
-        
-        for (int i = 0; i < discount.length - 9; i++) {
-            HashMap<String, Integer> map = new HashMap<>();
-            for (int j = 0; j < 10; j++) {
-                map.put(discount[i+j], map.getOrDefault(discount[i+j], 0) + 1);
-            }
-            boolean flag = false;
-            // System.out.println(map);
-            for (int j = 0; j < want.length; j++) {
-                if (map.getOrDefault(want[j], 0) < number[j]) {
-                    // System.out.println(want[j] + " " + map.getOrDefault(want[j], 0) + " " + number[j]);
-                    flag = true;
-                    continue;
-                }
-            }
-            if (!flag) {
-                answer++;
-            }
-        }
-        
         return answer;
     }
 }
