8cdf5857000ed991dbc9630bdf4fed20bdad1bbd
Sangmin Park <70051888+smart-sangmin@users.noreply.github.com>
2023-04-28 21:01:14 +0900

    [unrated] Title: 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/unrated/164671. 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기/조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기.sql
+++ 프로그래머스/unrated/164671. 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기/조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기.sql
@@ -1,12 +1,6 @@
--- 코드를 입력하세요
-SELECT concat("/home/grep/src/", b.BOARD_ID, "/", FILE_ID, FILE_NAME, FILE_EXT) file_path
-from used_goods_board b
-join used_goods_file f
-on b.board_id = f.board_id
-where b.views = (
-    select *
-    from (select views
-    from used_goods_board
-    order by views desc limit 1) as sub
-) 
-order by file_id desc
+select concat("/home/grep/src/", BOARD_ID, "/", FILE_ID, FILE_NAME, FILE_EXT) FILE_PATH
+from USED_GOODS_BOARD B
+join USED_GOODS_FILE F using(BOARD_ID)
+where VIEWS = (select MAX(VIEWS) from USED_GOODS_BOARD)
+order by FILE_ID desc
+
