fb2debf0da1c042ae82cf3221071a25028d23354
Sangmin Park <70051888+smart-sangmin@users.noreply.github.com>
2023-05-04 09:06:53 +0900

    [level 2] Title: 의상, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42578. 의상/의상.py
@@ -0,0 +1,11 @@
+def solution(clothes):
+    answer = 1
+    dic = {}
+    for i in clothes:
+        if i[1] in dic.keys():
+            dic[i[1]] += 1
+        else:
+            dic[i[1]] = 1
+    for i in dic.values():
+        answer *= (i + 1)
+    return answer - 1
