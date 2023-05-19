d9da5bccda66baf0efd3a63bc019b45e0fe0a85f
Sangmin Park <70051888+smart-sangmin@users.noreply.github.com>
2023-05-20 01:58:07 +0900

    [Gold IV] Title: 발전소 설치, Time: 732 ms, Memory: 50384 KB -BaekjoonHub

--- 백준/Gold/1277. 발전소 설치/발전소 설치.java
+++ 백준/Gold/1277. 발전소 설치/발전소 설치.java
@@ -1,87 +1,79 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.PriorityQueue;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    int[][] plants;
-    Double[][] dist;
+    static double[][] graph;
+    static HashMap<Integer, Integer[]> locations;
+    static double INF = 200000;
 
-    void input() throws IOException {
+    public static void main(String args[]) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int w = Integer.parseInt(st.nextToken());
-        plants = new int[n][2];
-        dist = new Double[n][n];
-        double m = Double.parseDouble(br.readLine());
-        for (int i = 0; i < n; i++) {
+        int N = Integer.parseInt(st.nextToken());
+        int W = Integer.parseInt(st.nextToken());
+        double M = Double.parseDouble(br.readLine());
+        locations = new HashMap<>();
+        graph = new double[N][N];
+        for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
-            plants[i][0] = Integer.parseInt(st.nextToken());
-            plants[i][1] = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            int x = Integer.parseInt(st.nextToken());
+            locations.put(i, new Integer[]{y, x});
+            Arrays.fill(graph[i], INF);
             for (int j = 0; j < i; j++) {
-                double d = dSquare(plants[i], plants[j]);
-                if (d <= m * m) dist[i][j] = dist[j][i] = Math.sqrt(d);
+                double d = getDistance(i, j);x
+                if (d <= M){ 
+                    graph[i][j] = graph[j][i] = d;
+                }
             }
+            graph[i][i] = 0;
         }
-        while (w-- > 0) {
+        
+        for (int i = 0; i < W; i++) {
             st = new StringTokenizer(br.readLine());
-            int start = Integer.parseInt(st.nextToken()) - 1;
-            int end = Integer.parseInt(st.nextToken()) - 1;
-            dist[start][end] = dist[end][start] = 0.0;
+            int a = Integer.parseInt(st.nextToken())-1;
+            int b = Integer.parseInt(st.nextToken())-1;
+            graph[a][b] = graph[b][a] = 0;
         }
-    }
-
-    double dSquare(int[] p1, int[] p2) {
-        double d = (double) (p1[0] - p2[0]) * (p1[0] - p2[0]);
-        d += (double) (p1[1] - p2[1]) * (p1[1] - p2[1]);
-        return d;
-    }
-
-    double Dijkstra() {
-        double[] minDist = new double[plants.length];
-        for (int i = 0; i < plants.length; i++) {
-            minDist[i] = Double.MAX_VALUE;
-        }
-        class Plant implements Comparable<Plant> {
-            final int plantNo;
-            final double dist;
-
-            Plant(int plantNo, double dist) {
-                this.plantNo = plantNo;
-                this.dist = dist;
+        
+        PriorityQueue<double[]> pq = new PriorityQueue<>(Comparator.comparingDouble(o -> o[1]));
+        pq.offer(new double[]{0, 0});
+        double[] dijkstra = new double[N];
+        Arrays.fill(dijkstra, INF);
+        while(!pq.isEmpty()) {
+            double[] temp = pq.poll();
+            int node = (int) temp[0];
+            double dist = temp[1];
+            if (dist > graph[0][node]) {
+                continue;
             }
-
-            @Override
-            public int compareTo(Plant p) {
-                return Double.compare(this.dist, p.dist);
-            }
-        }
-        PriorityQueue<Plant> pq = new PriorityQueue<>();
-        pq.add(new Plant(0, 0));
-        while (!pq.isEmpty()) {
-            Plant curr = pq.poll();
-            int currPlantNo = curr.plantNo;
-            double currDist = curr.dist;
-            if (minDist[currPlantNo] < currDist) continue;
-            for (int i = 0; i < plants.length; i++) {
-                if (dist[currPlantNo][i] == null) continue;
-                if (currDist + dist[currPlantNo][i] < minDist[i]) {
-                    minDist[i] = currDist + dist[currPlantNo][i];
-                    pq.add(new Plant(i, minDist[i]));
+            for (int i = 0; i < N; i++) {
+                if (dijkstra[i] > dist + graph[node][i]) {
+                    dijkstra[i] = dist + graph[node][i]; 
+                    pq.offer(new double[] {i, dijkstra[i]});
                 }
             }
         }
-        return minDist[plants.length - 1];
+        long answer = (long) (dijkstra[N-1] * 1000);;
+        if (dijkstra[N-1] == INF) {
+            answer = -1;
+        }
+        System.out.println(answer);
     }
 
-    void solution() throws IOException {
-        input();
-        System.out.println((int) (Dijkstra() * 1000));
+    public static double getDistance(int a, int b) {
+        Integer[] aLocation = locations.get(a);
+        Integer[] bLocation = locations.get(b);
+        return Math.sqrt(Math.pow(aLocation[0] - bLocation[0], 2) + Math.pow(aLocation[1] - bLocation[1], 2));
     }
 
-    public static void main(String[] args) throws IOException {
-        new Main().solution();
+    public static void printGraph() {
+        int N = graph.length;
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                System.out.printf("%10.1f", graph[i][j]);
+            }
+            System.out.println();
+        }
     }
 }
