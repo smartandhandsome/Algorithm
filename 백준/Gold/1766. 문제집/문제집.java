bf2cdfa493ab6975e84431246c19ac850bd210bd
Sangmin Park <70051888+smart-sangmin@users.noreply.github.com>
2023-05-20 21:43:02 +0900

    [Gold II] Title: 문제집, Time: 912 ms, Memory: 50560 KB -BaekjoonHub

--- 백준/Gold/1766. 문제집/문제집.java
+++ 백준/Gold/1766. 문제집/문제집.java
@@ -5,17 +5,15 @@ public class Main {
     static int[] indegree;
     static ArrayList<Integer>[] relation;
     static ArrayList<Integer> order;
-    static boolean[] solved;
-    
+
     public static void main(String args[]) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
-        
+
         indegree = new int[N];
         relation = new ArrayList[N];
-        solved = new boolean[N];
         for (int i = 0; i < N; i++) {
             relation[i] = new ArrayList<>();
         }
@@ -26,30 +24,27 @@ public class Main {
             relation[A].add(B);
             indegree[B]++;
         }
-        
+
         order = new ArrayList<>();
         PriorityQueue<Integer> q = new PriorityQueue<>();
-        addZero(q);
+        for (int i = 0; i < N; i++) {
+            if (indegree[i] == 0) { 
+                q.offer(i);
+            }
+        }
         while(!q.isEmpty()) {
             int n = q.poll();
             order.add(n);
             for (int next : relation[n]) {
                 indegree[next]--;
+                if (indegree[next] == 0) {
+                    q.offer(next);
+                }
             }
-            addZero(q);
         }
+
         for (int i : order) {
             System.out.print((i+1) + " ");
-        }
-    }
-    
-    public static void addZero(Queue<Integer> q) {
-        int N = indegree.length;
-        for (int i = 0; i < N; i++) {
-            if (indegree[i] == 0  && !solved[i]) {
-                q.add(i);
-                solved[i] = true;
-            }
         }
     }
 }
