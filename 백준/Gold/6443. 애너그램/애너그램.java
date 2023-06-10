4d85654018097aadacb90439eecadb42ee41aa9a
Sangmin Park <70051888+smart-sangmin@users.noreply.github.com>
2023-06-10 10:09:47 +0900

    [Gold V] Title: 애너그램, Time: 344 ms, Memory: 31064 KB -BaekjoonHub

--- 백준/Gold/6443. 애너그램/애너그램.java
+++ 백준/Gold/6443. 애너그램/애너그램.java
@@ -1,46 +1,48 @@
 import java.io.*;
-import java.util.*;
+import java.util.Arrays;
 
 public class Main {
-    static String S;
-    static int L;
-    static int[] cnt;
-    static List<String> answer;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int N = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
         for (int i = 0; i < N; i++) {
-            S = br.readLine();
-            L = S.length();
-            cnt = new int[26];
-            for (int l = 0; l < L; l++) {
-                cnt[S.charAt(l) - 'a']++;
-            }
-            answer = new ArrayList<>();
-            dfs(new StringBuilder());
-            for (String ans : answer) {
-                bw.write(ans);
-                bw.write("\n");
-            }
-            bw.flush();
+            char[] c = br.readLine().toCharArray();
+            Arrays.sort(c);
+            do {
+                sb.append(c).append("\n");
+            } while (nextPermutation(c));
         }
+        bw.write(sb.toString());
+        bw.flush();
         bw.close();
     }
 
-    public static void dfs(StringBuilder anagram) {
-        if (anagram.length() == L) {
-            answer.add(anagram.toString());
+    public static boolean nextPermutation(char[] c) {
+        int i = c.length - 1;
+        while (i > 0 && c[i] <= c[i - 1]) {
+            i--;
         }
-        for (int i = 0; i < 26; i++) {
-            if (cnt[i] > 0) {
-                cnt[i]--;
-                anagram.append((char) (i + 'a'));
-                dfs(anagram);
-                anagram.deleteCharAt(anagram.length() - 1);
-                cnt[i]++;
-            }
+        if (i == 0) {
+            return false;
         }
+        int j = c.length - 1;
+        while (c[i - 1] >= c[j]) {
+            j--;
+        }
+        swap(c, i-1, j);
+
+        int k = c.length - 1;
+        while (i <= k) {
+            swap(c, i++, k--);
+        }
+        return true;
+    }
+
+    public static void swap(char[] chars, int a, int b) {
+        char temp = chars[a];
+        chars[a] = chars[b];
+        chars[b] = temp;
     }
 }
