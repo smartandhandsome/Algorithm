5698a06883bd3055447b1a48e0f6686cd6d27d05
Sangmin Park <70051888+smart-sangmin@users.noreply.github.com>
2023-06-18 14:33:28 +0900

    [Gold II] Title: 나누기, Time: 268 ms, Memory: 23968 KB -BaekjoonHub

--- 백준/Gold/21757. 나누기/나누기.java
+++ 백준/Gold/21757. 나누기/나누기.java
@@ -8,7 +8,6 @@ public class Main {
     static int N;
     static long[] nums;
     static long[] sum;
-    static long part;
     static long count;
 
     public static void main(String[] args) throws IOException {
@@ -30,30 +29,29 @@ public class Main {
 
         if (sum[N - 1] % 4 != 0) {
             System.out.println(0);
-        } else if (sum[N - 1] == 0) {
+        } else if (sum[N - 1] == 0) {  // 누적합이 0일때 N-1 을 제외하고 zero 개수 세서 zeroC3(조합)
             int zero = 0;
             for (int i = 0; i < N - 1; i++) {
                 if (nums[i] == 0) {
                     zero += 1;
                 }
             }
-            count = (long) zero * (zero - 1) * (zero - 2) / (3 * 2); // zeroCr
+            count = (long) zero * (zero - 1) * (zero - 2) / (3 * 2); // zero C 3
             System.out.println(count);
         } else {
-            part = sum[N - 1] / 4;
-            dfs(0, part);
-            System.out.println(count);
-        }
-    }
-
-    private static void dfs(int idx, long target) {
-        if (idx == N && target == part * 5) {
-            count++;
-        }
-        for (int i = idx; i < N; i++) {
-            if (sum[i] == target) {
-                dfs(i + 1, target + part);
+            long[] dp = {1, 0, 0, 0, 0}; // 0, 1구간 2구간 3구간 4구간
+            long part = sum[N - 1] / 4;
+            for (int i = 0; i < N; i++) {
+                if (sum[i] % part != 0) {
+                    continue;
+                }
+                int d = (int) (sum[i] / part);
+                if (d < 1 || d >= 4) {
+                    continue;
+                }
+                dp[d] += dp[d - 1];
             }
+            System.out.println(dp[4]);
         }
     }
 }
