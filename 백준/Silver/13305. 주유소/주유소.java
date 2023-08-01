807fdcf86a174a6db1120f804ca6b057f32fc276
Sangmin Park <70051888+smart-sangmin@users.noreply.github.com>
2023-08-02 03:36:57 +0900

    [Silver III] Title: 주유소, Time: 532 ms, Memory: 46260 KB -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.java
+++ 백준/Silver/13305. 주유소/주유소.java
@@ -3,27 +3,28 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.List;
-import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicLong;
 import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        int answer = 0;
+        long answer = 0;
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        AtomicInteger sum = new AtomicInteger();
-        int N = Integer.parseInt(br.readLine());
-        List<Integer> sumDistance = Arrays.stream(br.readLine().split(" "))
-                .map(s -> sum.addAndGet(Integer.parseInt(s)))
+        int n = Integer.parseInt(br.readLine());
+
+        AtomicLong sum = new AtomicLong();
+        List<Long> sumDistance = Arrays.stream(br.readLine().split(" "))
+                .map(s -> sum.addAndGet(Long.parseLong(s)))
                 .collect(Collectors.toList());
 
-        List<Integer> oilPricePerLiter = Arrays.stream(br.readLine().split(" "))
-                .map(Integer::parseInt)
+        List<Long> oilPricePerLiter = Arrays.stream(br.readLine().split(" "))
+                .map(Long::parseLong)
                 .collect(Collectors.toList());
 
 
         int arrived = 0;
         int pivot = arrived + 1;
-        while (pivot < N - 1) {
+        while (pivot < n - 1) {
             if (oilPricePerLiter.get(pivot) < oilPricePerLiter.get(arrived)) {
                 answer += oilPricePerLiter.get(arrived) *
                         (sumDistance.get(pivot - 1) - (arrived == 0 ? 0 : sumDistance.get(arrived - 1)));
@@ -32,9 +33,9 @@ public class Main {
             pivot++;
         }
 
-        if (arrived != N - 1) {
+        if (arrived != n - 1) {
             answer += oilPricePerLiter.get(arrived) *
-                    (sumDistance.get(N - 2) - (arrived == 0 ? 0 : sumDistance.get(arrived - 1)));
+                    (sumDistance.get(n - 2) - (arrived == 0 ? 0 : sumDistance.get(arrived - 1)));
         }
 
         System.out.println(answer);
