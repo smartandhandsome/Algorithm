f63d36611d41b463225f17c218dfe82cd23a1943
Sangmin Park <70051888+smart-sangmin@users.noreply.github.com>
2023-08-25 00:05:15 +0900

    [Gold II] Title: 모래성, Time: 604 ms, Memory: 101940 KB -BaekjoonHub

--- 백준/Gold/10711. 모래성/모래성.java
+++ 백준/Gold/10711. 모래성/모래성.java
@@ -1,104 +1,103 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
 
 public class Main {
-	static int h, w;
-	static char[][] map;
-	static boolean[][] visited;
-	static ArrayList<Point> list = new ArrayList<>(); // 모래성 좌표
-	static int[][] move = { { -1, 0 }, { -1, 1 }, { 0, 1 }, { 1, 1 }, { 1, 0 }, { 1, -1 }, { 0, -1 }, { -1, -1 } };
-	static Queue<Point> q = new LinkedList<>();
+    private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static final int[][] directions = {{-1, -1}, {-1, 0}, {-1, 1}, {0, -1}, {0, 1}, {1, -1}, {1, 0}, {1, 1}};
+    private static int[] size;
+    private static int[][] cattle;
+    private static boolean[][] zero;
 
-	public static void main(String[] args) throws IOException {
-		// TODO Auto-generated method stub
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String[] temp = br.readLine().split(" ");
-		h = Integer.parseInt(temp[0]);
-		w = Integer.parseInt(temp[1]);
-		map = new char[h][w];
-		visited = new boolean[h][w];
-		for (int i = 0; i < h; i++) {
-			String line = br.readLine();
-			for (int j = 0; j < w; j++) {
-				map[i][j] = line.charAt(j);
-			}
-		}
-		for (int i = 0; i < h; i++) {
-			for (int j = 0; j < w; j++) {
-				if (map[i][j] == '.' || map[i][j] == '9') {
-					visited[i][j] = true;
-					continue;
-				}
-				int num = map[i][j] - '0';
-				if (check(new Point(i, j)) >= num) {
-					q.add(new Point(i, j));
-					visited[i][j] = true;
-				}
-			}
-		}
-		bfs();
-	}
+    public static void main(String[] args) throws IOException {
+        size = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        zero = new boolean[size[0]][size[1]];
+        cattle = makeCattle();
 
-	private static void bfs() {
-		// TODO Auto-generated method stub
-		char[][] tempMap = new char[h][w];
-		copy(tempMap);
-		int time;
-		for (time = 0; !q.isEmpty(); time++) {
-			int q_size = q.size();
-			for (int i = 0; i < q_size; i++) {
-				Point p = q.poll(); // 무너뜨린 모래성
-				map[p.x][p.y] = '.';
+        Queue<int[]> q = firstWave();
+        int count = q.isEmpty() ? 0 : 1;
+        while (true) {
+            q = wave(q);
+            if (q.isEmpty()) {
+                break;
+            }
+            count++;
+        }
+        System.out.println(count);
+    }
 
-				for (int j = 0; j < move.length; j++) {
-					int x = p.x + move[j][0];
-					int y = p.y + move[j][1];
-					int num = map[x][y] - '0';
-					if (isIn(x, y) && tempMap[x][y] != '.' && !visited[x][y] && check(new Point(x, y)) >= num) {
-						q.add(new Point(x, y));
-						visited[x][y] = true;
-					}
-				}
-			}
-		}
-		System.out.println(time);
-	}
+    private static Queue<int[]> wave(Queue<int[]> q) {
+        Queue<int[]> newQ = new LinkedList<>();
+        while (!q.isEmpty()) {
+            int[] brokenPoint = q.poll();
+            cattle[brokenPoint[0]][brokenPoint[1]] = 0;
+            for (int[] direction : directions) {
+                int y = brokenPoint[0] + direction[0];
+                int x = brokenPoint[1] + direction[1];
+                if (!zero[y][x] && isBroken(y, x)) {
+                    newQ.add(new int[]{y, x});
+                    zero[y][x] = true;
+                }
+            }
+        }
+        return newQ;
+    }
 
-	private static int check(Point p) {
-		// TODO Auto-generated method stub
-		int cnt = 0;
-		for (int j = 0; j < move.length; j++) {
-			int x = p.x + move[j][0];
-			int y = p.y + move[j][1];
-			if (isIn(x, y) && map[x][y] == '.')
-				cnt++;
-		}
-		return cnt;
-	}
+    private static Queue<int[]> firstWave() {
+        Queue<int[]> q = new LinkedList<>();
+        for (int i = 1; i < size[0] - 1; i++) {
+            for (int j = 1; j < size[1] - 1; j++) {
+                if (!zero[i][j] && isBroken(i, j)) {
+                    q.add(new int[]{i, j});
+                    zero[i][j] = true;
+                }
+            }
+        }
+        return q;
+    }
 
-	private static boolean isIn(int x, int y) {
-		// TODO Auto-generated method stub
-		return x >= 0 && y >= 0 && x < h && y < w;
-	}
+    private static boolean isBroken(int y, int x) {
+        int count = 0;
+        for (int[] direction : directions) {
+            int checkY = y + direction[0];
+            int checkX = x + direction[1];
+            if (cattle[checkY][checkX] == 0) {
+                count++;
+                if (count == cattle[y][x]) {
+                    break;
+                }
+            }
+        }
+        return count == cattle[y][x];
+    }
 
-	private static void copy(char[][] tempMap) {
-		for (int i = 0; i < h; i++) {
-			for (int j = 0; j < w; j++) {
-				tempMap[i][j] = map[i][j];
-			}
-		}
-	}
+    private static int[][] makeCattle() throws IOException {
+        int[][] cattle = new int[size[0]][size[1]];
+        for (int i = 0; i < size[0]; i++) {
+            String temp = br.readLine();
+            for (int j = 0; j < size[1]; j++) {
+                char c = temp.charAt(j);
+                if (c == '.') {
+                    c = '0';
+                    zero[i][j] = true;
+                }
+                if (c == '9') {
+                    zero[i][j] = true;
+                }
+                cattle[i][j] = c - '0';
+            }
+        }
+        return cattle;
+    }
 
-	private static class Point {
-		int x, y;
-
-		Point(int x, int y) {
-			this.x = x;
-			this.y = y;
-		}
-	}
+    private static int[][] cloneCattle() {
+        int[][] newCattle = new int[size[0]][size[1]];
+        for (int i = 0; i < size[0]; i++) {
+            newCattle[i] = cattle[i].clone();
+        }
+        return newCattle;
+    }
 }
