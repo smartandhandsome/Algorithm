059cfbc9b8caaa86c533bf571bf6f22efe9eabbf
Sangmin Park <70051888+smart-sangmin@users.noreply.github.com>
2023-08-26 05:21:54 +0900

    [Gold V] Title: 개업, Time: 308 ms, Memory: 16280 KB -BaekjoonHub

--- 백준/Gold/13910. 개업/개업.java
+++ 백준/Gold/13910. 개업/개업.java
@@ -2,43 +2,37 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.StringTokenizer;
 
 public class Main {
+    private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static final int MAX = 10001;
+
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
-        int[] woks = new int[M];
-        int[] dp = new int[N + 1];
-        Arrays.fill(dp, 10001);
-        st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < M; i++) {
-            woks[i] = Integer.parseInt(st.nextToken());
-            if (woks[i] <= N) {
-                dp[woks[i]] = 1; // 한 손
-            }
-        }
-        // 양손
-        for (int i = 0; i < M; i++) {
-            for (int j = 0; j < M; j++) {
-                if (woks[i] + woks[j] <= N && i != j) {
-                    dp[woks[i] + woks[j]] = 1;
+        String[] s = br.readLine().split(" ");
+        int target = Integer.parseInt(s[0]);
+        int[] dp = new int[target + 1];
+        Arrays.fill(dp, MAX);
+        int[] woks = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        Arrays.stream(woks).forEach(value -> dp[value] = 1);
+
+        for (int i = 0; i < woks.length; i++) {
+            for (int j = i + 1; j < woks.length; j++) {
+                if (woks[i] + woks[j] > target) {
+                    continue;
                 }
+                dp[woks[i] + woks[j]] = 1;
             }
         }
 
-        // 횟수 증가
-        for (int i = 0; i <= N; i++) {
-            for (int j = 0; j <= N; j++) {
+        for (int i = 0; i <= target; i++) {
+            for (int j = 0; j <= target; j++) {
                 if (i - j < 0) {
                     continue;
                 }
                 dp[i] = Math.min(dp[i], dp[i - j] + dp[j]);
             }
         }
-//        System.out.println(Arrays.toString(dp));
-        System.out.println(dp[N] == 10001 ? -1 : dp[N]);
+
+        System.out.println(dp[target] == 10001 ? -1 : dp[target]);
     }
 }
