7dd14cf070d93b4dc58fa835eb7b7f384bf863a5
Sangmin Park <70051888+smartandhandsome@users.noreply.github.com>
2023-12-15 23:51:15 +0900

    [level unrated] Title: 길이에 따른 연산, Time: 0.70 ms, Memory: 73.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181879. 길이에 따른 연산/길이에 따른 연산.java
+++ 프로그래머스/unrated/181879. 길이에 따른 연산/길이에 따른 연산.java
@@ -1,18 +1,9 @@
 import java.util.Arrays;
-import java.util.concurrent.atomic.AtomicInteger;
 
 class Solution {
     public int solution(int[] num_list) {
-        AtomicInteger answer = new AtomicInteger();
-        if (num_list.length < 11) {
-            AtomicInteger a = new AtomicInteger(1);
-            Arrays.stream(num_list)
-                    .forEach(value -> a.updateAndGet(v -> v * value));
-            answer.set(a.get());
-        } else {
-            Arrays.stream(num_list)
-                    .forEach(answer::addAndGet);
-        }
-        return answer.get();
+        return num_list.length < 11 ?
+                Arrays.stream(num_list).reduce(1, (left, right) -> left * right) :
+                Arrays.stream(num_list).sum();
     }
 }
