b681495367efe5b958544284de5ed8701f922bff
Sangmin Park <70051888+smartandhandsome@users.noreply.github.com>
2024-01-02 23:51:53 +0900

    [level 2] Title: 택배 배달과 수거하기, Time: 5.62 ms, Memory: 90 MB -BaekjoonHub

--- 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.java
+++ 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.java
@@ -2,19 +2,37 @@ class Solution {
     public long solution(int cap, int n, int[] deliveries, int[] pickups) {
         long answer = 0;
 
-        int d = 0;
-        int p = 0;
-        for (int i = n - 1; i >= 0; i--) {
-            d += deliveries[i];
-            p += pickups[i];
-            while (d > 0 || p > 0) {
-                d -= cap;
-                p -= cap;
-                
-                answer += (i+1) * 2;
+        int maxD = n - 1;
+        int maxP = n - 1;
+        int c = 0;
+        while (true) {
+            while(maxD >= 0 && deliveries[maxD] == 0) maxD--;
+            while(maxP >= 0 && pickups[maxP] == 0) maxP--;
+            if (maxD == -1 && maxP == -1) {
+                break;
             }
+            
+            int d = 0;
+            for (int i = maxD; i >= 0; i--) {
+                d += deliveries[i];
+                deliveries[i] = 0;
+                if (d >= cap) {
+                    deliveries[i] = d - cap;
+                    break;
+                }
+            }
+            
+            int p = 0;
+            for (int i = maxP; i >= 0; i--) {
+                p += pickups[i];
+                pickups[i] = 0;
+                if (p >= cap) {
+                    pickups[i] = p - cap;
+                    break;
+                }
+            }
+            answer += (Math.max(maxD, maxP) + 1) * 2;
         }
-        
         return answer;
     }
 }
