c5f4a1f06a9956f0396716f9e07fe0235d8eedaa
Sangmin Park <70051888+smartandhandsome@users.noreply.github.com>
2024-01-31 04:08:04 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 11.29 ms, Memory: 104 MB -BaekjoonHub

---
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.java
@@ -0,0 +1,46 @@
+import java.util.Arrays;
+
+public class Solution {
+    public int solution(int[] queue1, int[] queue2) {
+
+
+        int[] total = new int[queue1.length + queue2.length];
+        int n = total.length;
+        System.arraycopy(queue1, 0, total, 0, queue1.length);
+        System.arraycopy(queue2, 0, total, queue2.length, queue2.length);
+
+
+        int index1 = 0;
+        int index2 = queue1.length;
+
+        long sum1 = Arrays.stream(queue1)
+                .reduce(0, Integer::sum);
+        long sum2 = Arrays.stream(queue2)
+                .reduce(0, Integer::sum);
+        int answer = 0;
+        while (sum1 != sum2) {
+            if (sum1 > sum2) {
+                sum1 -= total[index1];
+                sum2 += total[index1];
+                index1++;
+                index1 %= n;
+            } else {
+                sum1 += total[index2];
+                sum2 -= total[index2];
+                index2++;
+                index2 %= n;
+            }
+            answer++;
+            if (answer >= n * 3) {
+                return -1;
+            }
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.solution(new int[]{3, 2, 7, 2}, new int[]{4, 6, 5, 1}));
+    }
+}
