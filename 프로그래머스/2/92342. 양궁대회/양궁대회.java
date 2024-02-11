f689bf95966075cd714d362f9e494efe475c13e9
Sangmin Park <70051888+smartandhandsome@users.noreply.github.com>
2024-02-11 21:31:08 +0900

    [level 2] Title: 양궁대회, Time: 9.07 ms, Memory: 80 MB -BaekjoonHub

---
+++ 프로그래머스/2/92342. 양궁대회/양궁대회.java
@@ -0,0 +1,66 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Solution {
+    static int[] answer = {-1};
+    static int maxV = Integer.MIN_VALUE;
+
+    public int[] solution(int n, int[] info) {
+        for (int i = 10; i >= 1; i--) {
+            dfs(10, new ArrayList<>(), i, info, n);
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        System.out.println(Arrays.toString(solution.solution(5, new int[]{2, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0})));
+    }
+
+    public static void dfs(int idx, List<Integer> lion, int max, int[] info, int n) {
+        if (lion.size() == max) {            
+            int count = 0;
+            int[] answer = new int[11];
+            for (int s : lion) {
+                int index = 10 - s;
+                answer[index] = info[index] + 1;
+                count += info[index] + 1;
+                if (count > n) {
+                    return;
+                }
+            }
+            
+            if (count != n) {
+                answer[10] += n - count;
+            }
+
+            int ls = 0;
+            int as = 0;
+            for (int i = 0; i < 11; i++) {
+                if (info[i] >= answer[i]) {
+                    if (info[i] == 0) {
+                        continue;
+                    }
+                    as += 10 - i;
+                } else {
+                    ls += 10 - i;
+                }
+            }
+            if (ls > as && ls - as > maxV) {
+                System.out.println(ls + " " + as);
+                System.out.println(lion);
+                maxV = ls - as;
+                Solution.answer = answer;
+            }
+            return;
+        }
+
+        for (int i = idx; i >= 1; i--) {
+            lion.add(i);
+            dfs(i - 1, lion, max, info, n);
+            lion.remove((Integer) i);
+        }
+    }
+}
