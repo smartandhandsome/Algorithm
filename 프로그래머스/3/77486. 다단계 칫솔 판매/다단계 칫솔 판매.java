3e5979b96f65cbde7ebb5716bb2849b484fa0793
Sangmin Park <70051888+smartandhandsome@users.noreply.github.com>
2024-02-24 18:49:21 +0900

    [level 3] Title: 다단계 칫솔 판매, Time: 71.34 ms, Memory: 140 MB -BaekjoonHub

---
+++ 프로그래머스/3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.java
@@ -0,0 +1,61 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(String[] enroll, String[] referral, String[] seller, int[] amount) {
+        
+        
+        int n = enroll.length;
+        
+        Map<String, String> parents = new HashMap<>();
+        Map<String, Integer> sales = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            sales.put(enroll[i], 0);
+            parents.put(enroll[i], referral[i]);
+        }
+        
+        int m = seller.length;
+        for (int i = 0; i < m; i++) {
+            calc(parents, sales, seller[i], amount[i] * 100);
+        }
+        
+        int[] answer = new int[n];
+        for (int i = 0; i < n; i++) {
+            answer[i] = sales.get(enroll[i]);
+        }
+        
+        return answer;
+    }
+    
+    static void calc(Map<String, String> parents, Map<String, Integer> sales, String sell, int total) {
+        String name = sell;
+        int money = total;
+        sales.put(name, sales.get(name) + money);
+        while(true){
+            String p = parents.get(name);
+            int ten = money / 10;
+            
+            sales.put(name, sales.get(name) - ten);
+            if(p.equals("-") || ten == 0) {
+                break;
+            }
+            sales.put(p, sales.get(p) + ten);
+            
+            name = p;
+            money = ten;
+        }
+    }
+    
+    static class Node {
+        String name;
+        int money;
+        
+        Node(String name, int money) {
+            this.name = name;
+            this.money = money;
+        }
+    }
+    
+    static void print(Object o) {
+        System.out.println(o);
+    }
+}
