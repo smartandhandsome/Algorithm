e902f091f139edac65893524b4be49ac24ccdd26
Sangmin Park <70051888+smartandhandsome@users.noreply.github.com>
2024-04-29 23:24:18 +0900

    [Silver III] Title: 주유소, Time: 532 ms, Memory: 46260 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.java
+++ 백준/Silver/13305. 주유소/주유소.java
@@ -10,9 +10,8 @@ public class Main {
     public static void main(String[] args) throws IOException {
         long answer = 0;
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-
         AtomicLong sum = new AtomicLong();
+        int N = Integer.parseInt(br.readLine());
         List<Long> sumDistance = Arrays.stream(br.readLine().split(" "))
                 .map(s -> sum.addAndGet(Long.parseLong(s)))
                 .collect(Collectors.toList());
@@ -24,7 +23,7 @@ public class Main {
 
         int arrived = 0;
         int pivot = arrived + 1;
-        while (pivot < n - 1) {
+        while (pivot < N - 1) {
             if (oilPricePerLiter.get(pivot) < oilPricePerLiter.get(arrived)) {
                 answer += oilPricePerLiter.get(arrived) *
                         (sumDistance.get(pivot - 1) - (arrived == 0 ? 0 : sumDistance.get(arrived - 1)));
@@ -33,9 +32,9 @@ public class Main {
             pivot++;
         }
 
-        if (arrived != n - 1) {
+        if (arrived != N - 1) {
             answer += oilPricePerLiter.get(arrived) *
-                    (sumDistance.get(n - 2) - (arrived == 0 ? 0 : sumDistance.get(arrived - 1)));
+                    (sumDistance.get(N - 2) - (arrived == 0 ? 0 : sumDistance.get(arrived - 1)));
         }
 
         System.out.println(answer);
